6deaf174c6cf525b0a6dd2b8e58b1cea30566bcc
Juan Alorro <jralorro93@gmail.com>
2018-12-18 19:05:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 var recipes = new Object({});
 var playlist = { 'Adele': "Hello"}; 
 
-function updateObjectWithKeyAndValue(object, key, value) {
-  var obj = {prop: 1};
-  
-  return Object.assign ({}, playlist, {key: value});
-}
+function updateObjectWithKeyAndValue ()
